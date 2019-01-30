a8fbb4930c65537eb34471b5a1ef0a63744da4bf
shenyuanyu <shenyuanyu@ghostcloud.cn>
2019-01-30 10:21:30 +0800

    完善配置免密登录脚本

--- miss_pwd.sh
+++ miss_pwd.sh
@@ -1,17 +1,25 @@
-#!/usr/bin/expect
+#!/bin/bash
 
-[ ! -f /root/.ssh/id_rsa.pub  ] && ssh-keygen -t rsa -P '' &>/dev/null  # 密钥对不存在则创建密钥
+set -x 
+[ ! -f /root/.ssh/id_rsa.pub  ] && ssh-keygen -t rsa -P '' &>/dev/null
+
 while read line;do
-        ip=`echo $line | cut -d " " -f1`             # 提取文件中的ip
+ 	ip=`echo $line | cut -d " " -f1`             # 提取文件中的ip
         user_name=`echo $line | cut -d " " -f2`      # 提取文件中的用户名
         pass_word=`echo $line | cut -d " " -f3`      # 提取文件中的密码
-        expect <<EOF
-        spawn ssh-copy-id -i /root/.ssh/id_rsa.pub $user_name@$ip   # 复制公钥到目标主机
-        expect {
-            "yes/no" { send "yes\n";exp_continue }     # expect 实现自动输入密码
-            "password" { send "$pass_word\n" }                                                                        
-        }
-        expect eof
-        EOF
-done < /root/host_ip.txt      # 读取存储ip的文件
-                                                 
+	if [ $user_name != $ip ]; then
+		user=$user_name
+	fi
+        if [ $pass_word != $ip ]; then
+                pwd=$pass_word
+        fi
+	expect -c "
+        	set timeout -1;
+        	spawn ssh-copy-id  $user@$ip;
+        	expect {
+        	        \"yes/no\" { send \"yes\r\" ;exp_contine; }
+        	       	\"password:\" { send \"$pwd\r\"; }
+        	};
+        	expect eof;
+        	"
+done < host_ip.txt # 读取存储ip的文件, 若多台主机用户名密码相同, 可将多台主机顺序编写, 然后只需设置第一行的用户名和密码
